e41749b8286f7e2501c3fa633c9f1efb53be6384
Welcome <Welcome@SivaChandran>
2025-06-15 19:53:48 +0530

    Code setup for CPS app

---
+++ src/main/java/com/myapp/cps/CpsApp.java
@@ -0,0 +1,11 @@
+package com.myapp.cps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpsApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CpsApp.class, args);
+    }
+}
